0c9b6ec50cb2551dcecacafe70d69ba784945953
Chris Peralta <chris99994@gmail.com>
2017-10-06 17:09:32 +0800

    Rewriting and tidying code for organizational purposes and readability

--- initial_import.R
+++ initial_import.R
@@ -1,6 +1,6 @@
-library(dplyr)
-library(readr)
+library(tidyverse)
 library(janitor)
+library(stringr)
 
 
 ### This script is to do initial import and initial cleaning of the data, then re-export it.
@@ -26,8 +26,7 @@ supply_intake <- purrr::map(file_names_supply, read_csv) %>%
     intake_date,
     source_of_supply_if_appropriate,
     provided_by,
-    last_revision_date,
-    price_this_morning
+    last_revision_date
   ) %>% 
   rename(
     food_category = fresh_food_category,
@@ -35,11 +34,31 @@ supply_intake <- purrr::map(file_names_supply, read_csv) %>%
     supply_intake = intake,
     data_from = intake_date,
     food_origin = source_of_supply_if_appropriate,
-    supplier = provided_by,
     date_reported = last_revision_date
   ) 
 
-write_csv(supply_intake, path = 'tidy_supply_intake.csv')
+#parse columns to correct types
+supply_intake$date_morning <- dmy(supply_intake$date_reported) - 1
+supply_intake$food_category <- as.factor(supply_intake$food_category)
+supply_intake$supply_intake <- as.double(supply_intake$supply_intake)
+
+
+# Sort the columns into a more logical order
+supply_intake <- supply_intake %>% 
+  select(date_morning, everything()) %>% 
+  select(-data_from, -date_reported) #all data is from 'yesterday'
+
+
+supply_intake <- supply_intake %>% 
+  remove_empty_rows() 
+
+supply_intake <- supply_intake %>% 
+  filter(!is.na(supply_intake) & supply_intake != 0) #removes row with missing values and days with no intake
+# which is not useful information
+
+write_csv(supply_intake, path = 'tidied_supply_intake.csv')
+
+
 
 
 
@@ -49,35 +68,59 @@ file_names <- stringr::str_c('original_price_data', file_names, sep = '/')
 
 #Reads all .csv files, then combines all of the data frames
 wholesale_price <- purrr::map(file_names, read_csv) %>% 
-  bind_rows()
+  bind_rows() %>% 
+  clean_names() %>% 
+  remove_empty_rows() %>% 
+  remove_empty_cols() %>% 
+  select(
+    fresh_food_category,
+    food_type,
+    unit,
+    intake_date,
+    source_of_supply_if_appropriate,
+    provided_by,
+    last_revision_date,
+    price_this_morning
+  ) %>% 
+  rename(
+    food_category = fresh_food_category,
+    food_item = food_type,
+    price_morning = price_this_morning,
+    data_from = intake_date,
+    food_origin = source_of_supply_if_appropriate,
+    date_reported = last_revision_date
+  )
 
+# Parse columns into correct types
+wholesale_price$food_category <- as.factor(wholesale_price$food_category)
+wholesale_price$date_morning <- dmy(wholesale_price$date_reported) - 1 #convert reported date to date that the data if from
+wholesale_price$price_morning <- as.double(wholesale_price$price_morning)
 
-
-# Removed all Chinese data as the data is identical between columns
-# Removed 'ENGLISH CATEGORY' as 'INTAKE' is only valid for the 'Supply', therefore the 'ENGLISH CATEGORY' is redundant 
+# Sort the columns into a more logical order
 wholesale_price <- wholesale_price %>% 
-  select(c('FRESH FOOD CATEGORY', 
-           'FOOD TYPE', 
-           'PRICE (THIS MORNING)', 
-           'UNIT', 
-           'INTAKE DATE', 
-           'SOURCE OF SUPPLY (IF APPROPRIATE)', 
-           'PROVIDED BY', 
-           'Last Revision Date', 
-           'INTAKE'))
+  select(date_morning, everything()) %>% 
+  select(-data_from, -date_reported) #all data is from 'yesterday'
 
-    
-# Renames all of the variables to be more friendly
+
+#string manipulation on supplier & units
+wholesale_price$provided_by <- 
+  str_replace(wholesale_price$provided_by, "Major wholesalers af the Government Cheung Sha Wan and Western Wholesale 
+              Food Markets", 'Major wholesalers of the Government Cheung Sha Wan and Western Wholesale Food Markets')
+
+#removes rows with no price information
 wholesale_price <- wholesale_price %>% 
-  rename(food_category = 'FRESH FOOD CATEGORY', 
-         food_item = 'FOOD TYPE',
-         price_morning = 'PRICE (THIS MORNING)',
-         unit = 'UNIT', 
-         data_from = 'INTAKE DATE', 
-         food_origin = 'SOURCE OF SUPPLY (IF APPROPRIATE)', 
-         supplier = 'PROVIDED BY',
-         date_reported = 'Last Revision Date', 
-         supply_intake = 'INTAKE')
+  filter(!is.na(price_morning)) 
 
-write_csv(wholesale_price, path = 'tidy_wholesale_price.csv')
 
+
+write_csv(wholesale_price, path = 'tidied_wholesale_price.csv')
+
+wholesale_price <- read_csv('tidied_wholesale_price.csv', na = c('-', 'NA'), quoted_na = FALSE)
+supply_intake <- read_csv('tidied_supply_intake.csv', na = c('-', 'NA'), quoted_na = FALSE)
+
+
+supply_intake <- supply_intake %>% 
+  janitor::remove_empty_rows() %>% 
+  select(date_reported, everything())
+wholesale_price <- wholesale_price %>% 
+  janitor::remove_empty_rows()
